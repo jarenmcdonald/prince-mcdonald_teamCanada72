76654a577feba19a4f2423c506b3c8869352ad8d
Andrew Prince <andrewjoeprince@gmail.com>
2017-04-01 16:51:57 -0400

    Finished mobile Nav
    Finished custom mobile NAV bar

--- website/js/app.js
+++ website/js/app.js
@@ -1,1 +1,17 @@
-$(document).foundation();
+(function(){
+	"use strict";
+
+var content = document.getElementById("content");
+var button = document.getElementById("hamburger");
+
+button.onclick = function(e) {
+e.preventDefault()
+	if(content.className == "open"){
+		content.className = "";
+	} else {
+		content.className = "open";
+	}
+
+};
+
+})();
