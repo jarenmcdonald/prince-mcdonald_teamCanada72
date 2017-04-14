42240d0f278fe3c3c3f36613ccdebbdc6ec8f292
jarenmcdonald <estyn72@gmail.com>
2017-04-13 20:23:27 -0400

    JS updates

--- website/js/map.js
+++ website/js/map.js
@@ -10,14 +10,14 @@ $(document).foundation();
 	TweenMax.to(circles[1], 1, {delay: .5, left: "28%", ease: Bounce.easeOut});
 
 	function showInfo(e) {
-		//console.log("showin' it");
-		//console.log(e.currentTarget.dataset.box);
+		console.log("showin' it");
+		console.log(e.currentTarget.dataset.box);
 		var boxNum = e.currentTarget.dataset.box;
 		TweenMax.to(infoBoxes[boxNum],.5, {autoAlpha:1});
 	}
 
 	function hideInfo(e) {
-		//console.log("hidn' it");
+		console.log("hidn' it");
 		var boxNum = e.currentTarget.dataset.box;
 		TweenMax.to(infoBoxes[boxNum],1, {autoAlpha:0});
 	}
